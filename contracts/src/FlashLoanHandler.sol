845bfc230f4aec284c0a17ee566588a6f50fe2b3
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2025-05-12 04:01:59 -0600

    added crossl2inbox validation of messages

--- contracts/src/FlashLoanHandler.sol
+++ contracts/src/FlashLoanHandler.sol
@@ -7,19 +7,52 @@ import {FlashLoanVault} from "./FlashLoanVault.sol";
 import {ISuperchainWETH} from "@interop-lib/interfaces/ISuperchainWETH.sol";
 import {IL2ToL2CrossDomainMessenger} from "@interop-lib/interfaces/IL2ToL2CrossDomainMessenger.sol";
 import {CrossDomainMessageLib} from "@interop-lib/libraries/CrossDomainMessageLib.sol";
+import {PredeployAddresses} from "@interop-lib/libraries/PredeployAddresses.sol";
+import {Identifier, ICrossL2Inbox} from "@interop-lib/interfaces/ICrossL2Inbox.sol";
+
+error IdOriginNotFlashLoanHandler();
 
 contract FlashLoanHandler {
-
-    event flashLoanRecieved(bytes32 indexed flashLoanId, uint256 ethAmount, uint256 chainid, address indexed user);
-    event flashLoanRepayed(bytes32 indexed flashLoanId, uint256 ethAmount, uint256 chainid, address indexed user);
-    event soldEth(bytes32 indexed flashLoanId, uint256 ethAmount, uint256 chainid, address indexed user);
-    event boughtEth(bytes32 indexed flashLoanId, uint256 ethAmount, uint256 chainid, address indexed user);
-    event sentProfit(bytes32 indexed flashLoanId, uint256 ethAmount, uint256 chainid, address indexed user);
-    event noProfit(bytes32 indexed flashLoanId, uint256 chainid, address indexed user);
+    event flashLoanRecieved(
+        bytes32 indexed flashLoanId,
+        uint256 ethAmount,
+        uint256 chainid,
+        address indexed user
+    );
+    event flashLoanRepayed(
+        bytes32 indexed flashLoanId,
+        uint256 ethAmount,
+        uint256 chainid,
+        address indexed user
+    );
+    event soldEth(
+        bytes32 indexed flashLoanId,
+        uint256 ethAmount,
+        uint256 chainid,
+        address indexed user
+    );
+    event boughtEth(
+        bytes32 indexed flashLoanId,
+        uint256 ethAmount,
+        uint256 chainid,
+        address indexed user
+    );
+    event sentProfit(
+        bytes32 indexed flashLoanId,
+        uint256 ethAmount,
+        uint256 chainid,
+        address indexed user
+    );
+    event noProfit(
+        bytes32 indexed flashLoanId,
+        uint256 chainid,
+        address indexed user
+    );
 
     address payable flashBorrowerDefaultAddress;
 
-    address payable superchainWEthAddress = payable(0x4200000000000000000000000000000000000024);
+    address payable superchainWEthAddress =
+        payable(0x4200000000000000000000000000000000000024);
     ISuperchainWETH superchainWEth = ISuperchainWETH(superchainWEthAddress);
     IL2ToL2CrossDomainMessenger public constant messenger =
         IL2ToL2CrossDomainMessenger(0x4200000000000000000000000000000000000023);
@@ -33,10 +66,17 @@ contract FlashLoanHandler {
         flashBorrowerDefaultAddress = payable(_flashBorrowerAddress);
     }
 
-    function recieveEthForArbitrageSourceChain(uint256 destinationChain, address caller, uint256 laonAmount, bytes32 flashLoanId, address payable flashBorrower) 
-    public payable {
-        bytes32 sendEthMsgHash = superchainWEth.sendETH{value: address(this).balance}(address(this), destinationChain);
-        
+    function recieveEthForArbitrageSourceChain(
+        uint256 destinationChain,
+        address caller,
+        uint256 laonAmount,
+        bytes32 flashLoanId,
+        address payable flashBorrower
+    ) public payable {
+        bytes32 sendEthMsgHash = superchainWEth.sendETH{
+            value: address(this).balance
+        }(address(this), destinationChain);
+
         messenger.sendMessage(
             destinationChain,
             address(this),
@@ -48,40 +88,72 @@ contract FlashLoanHandler {
                 laonAmount,
                 flashLoanId,
                 flashBorrower
-                )
+            )
         );
     }
 
-    function recieveEthForArbitrageDestinationChain(bytes32 sendEthMsgHash, uint256 sourceChain, address caller, uint256 loanAmount, bytes32 flashLoanId, address payable flashBorrower) 
-    external {
+    function recieveEthForArbitrageDestinationChain(
+        bytes32 sendEthMsgHash,
+        uint256 sourceChain,
+        address caller,
+        uint256 loanAmount,
+        bytes32 flashLoanId,
+        address payable flashBorrower
+    ) external {
         CrossDomainMessageLib.requireCrossDomainCallback();
         CrossDomainMessageLib.requireMessageSuccess(sendEthMsgHash);
 
-        try this.callOnFlashLoan(flashLoanId, caller, flashBorrower){} catch {
+        try this.callOnFlashLoan(flashLoanId, caller, flashBorrower) {} catch {
             //TODO: Define Proper Events For Failiure
         }
         tranferToSourceChain(sourceChain, caller, loanAmount, flashLoanId);
     }
 
-    function callOnFlashLoan(bytes32 flashLoanId, address caller, address payable flashBorrower) external {
+    function callOnFlashLoan(
+        bytes32 flashLoanId,
+        address caller,
+        address payable flashBorrower
+    ) external {
         uint256 ethAmount = address(this).balance;
 
-        try IFlashBorrower(flashBorrower).onFlashLoan{value: ethAmount}(ethAmount, address(this)){
-            emit soldEth(flashLoanId, address(this).balance, block.chainid, caller);
+        try
+            IFlashBorrower(flashBorrower).onFlashLoan{value: ethAmount}(
+                ethAmount,
+                address(this)
+            )
+        {
+            emit soldEth(
+                flashLoanId,
+                address(this).balance,
+                block.chainid,
+                caller
+            );
         } catch {
-            revert ("onFlashLoan Function Failed");
+            revert("onFlashLoan Function Failed");
         }
 
-        if (address(this).balance < ethAmount){
-            revert ("Sufficient Funds Were Not Returned");
-        } 
+        if (address(this).balance < ethAmount) {
+            revert("Sufficient Funds Were Not Returned");
+        }
 
-        emit boughtEth(flashLoanId, address(this).balance, block.chainid, caller);
+        emit boughtEth(
+            flashLoanId,
+            address(this).balance,
+            block.chainid,
+            caller
+        );
     }
 
-    function tranferToSourceChain(uint256 sourceChain, address caller, uint256 loanAmount, bytes32 flashLoanId) private {
-        bytes32 sendEthMsgHashBack = superchainWEth.sendETH{value: address(this).balance}(address(this), sourceChain);
-        
+    function tranferToSourceChain(
+        uint256 sourceChain,
+        address caller,
+        uint256 loanAmount,
+        bytes32 flashLoanId
+    ) private {
+        bytes32 sendEthMsgHashBack = superchainWEth.sendETH{
+            value: address(this).balance
+        }(address(this), sourceChain);
+
         messenger.sendMessage(
             sourceChain,
             address(this),
@@ -91,46 +163,107 @@ contract FlashLoanHandler {
                 caller,
                 loanAmount,
                 flashLoanId
-                )
-            );
+            )
+        );
     }
 
-    function recieveEthOnSourceChainFromDestinationChain(bytes32 sendEthMsgHash, address caller, uint256 loanAmount, bytes32 flashLoanId) external{
+    function recieveEthOnSourceChainFromDestinationChain(
+        bytes32 sendEthMsgHash,
+        address caller,
+        uint256 loanAmount,
+        bytes32 flashLoanId
+    ) external {
         CrossDomainMessageLib.requireCrossDomainCallback();
         CrossDomainMessageLib.requireMessageSuccess(sendEthMsgHash);
 
         int256 profit = int256(address(this).balance) - int256(loanAmount);
 
-        if(profit > 0){
-            flashLoanVaultAddress.call {value: loanAmount} ("");
-            emit flashLoanRepayed(flashLoanId, loanAmount, block.chainid, caller);
-            payable(caller).call {value: address(this).balance} ("");
-            emit sentProfit(flashLoanId, uint256(profit), block.chainid, caller);
-        }
-        else {
-            flashLoanVaultAddress.call {value: address(this).balance} ("");
-            emit flashLoanRepayed(flashLoanId, loanAmount, block.chainid, caller);
+        if (profit > 0) {
+            flashLoanVaultAddress.call{value: loanAmount}("");
+            emit flashLoanRepayed(
+                flashLoanId,
+                loanAmount,
+                block.chainid,
+                caller
+            );
+            payable(caller).call{value: address(this).balance}("");
+            emit sentProfit(
+                flashLoanId,
+                uint256(profit),
+                block.chainid,
+                caller
+            );
+        } else {
+            flashLoanVaultAddress.call{value: address(this).balance}("");
+            emit flashLoanRepayed(
+                flashLoanId,
+                loanAmount,
+                block.chainid,
+                caller
+            );
             emit noProfit(flashLoanId, block.chainid, caller);
         }
     }
 
-    function initFlashLoan(uint256 destinationChain, address payable flashBorrower, address caller) public {
-        require(destinationChain != block.chainid, "Destination Chain Cannot Be Same As Source Chain");
+    function initFlashLoan(
+        uint256 destinationChain,
+        address payable flashBorrower,
+        address caller
+    ) public {
+        require(
+            destinationChain != block.chainid,
+            "Destination Chain Cannot Be Same As Source Chain"
+        );
 
         uint256 loanAmountRecieved = flashLoanVault.processMaxLoanRequest();
-        bytes32 flashLoanId = keccak256(abi.encodePacked( loanAmountRecieved, caller, block.number ));
+        bytes32 flashLoanId = keccak256(
+            abi.encodePacked(loanAmountRecieved, caller, block.number)
+        );
 
-        emit flashLoanRecieved(flashLoanId, loanAmountRecieved, block.chainid, caller);
+        emit flashLoanRecieved(
+            flashLoanId,
+            loanAmountRecieved,
+            block.chainid,
+            caller
+        );
 
-        this.recieveEthForArbitrageSourceChain(destinationChain, caller, loanAmountRecieved, flashLoanId, flashBorrower);
+        this.recieveEthForArbitrageSourceChain(
+            destinationChain,
+            caller,
+            loanAmountRecieved,
+            flashLoanId,
+            flashBorrower
+        );
     }
 
-    function callFlashLoanHandler(uint256 destinationChain) public {
-        this.initFlashLoan(destinationChain, flashBorrowerDefaultAddress, msg.sender);
-    }
-    function callFlashLoanHandlerAdvanced(uint256 destinationChain, address flashBorrowerAddress) public {
-        this.initFlashLoan(destinationChain, payable(flashBorrowerAddress), msg.sender);
+    function callFlashLoanHandler(
+        Identifier calldata msgId,
+        bytes calldata msgData,
+        uint256 destinationChain
+    ) public {
+        if (msgId.origin != address(this)) revert IdOriginNotFlashLoanHandler();
+        ICrossL2Inbox(PredeployAddresses.CROSS_L2_INBOX).validateMessage(
+            msgId,
+            keccak256(msgData)
+        );
+
+        this.initFlashLoan(
+            destinationChain,
+            flashBorrowerDefaultAddress,
+            msg.sender
+        );
     }
 
-    receive() external payable{}
+    function callFlashLoanHandlerAdvanced(
+        uint256 destinationChain,
+        address flashBorrowerAddress
+    ) public {
+        this.initFlashLoan(
+            destinationChain,
+            payable(flashBorrowerAddress),
+            msg.sender
+        );
+    }
+
+    receive() external payable {}
 }
--- contracts/src/SimpleFlashLoan.sol
+++ contracts/src/SimpleFlashLoan.sol
@@ -1,58 +1,58 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.26;
 
-import "https://github.com/aave/aave-v3-core/blob/master/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
-import "https://github.com/aave/aave-v3-core/blob/master/contracts/interfaces/IPoolAddressesProvider.sol";
-import "https://github.com/aave/aave-v3-core/blob/master/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+// import "https://github.com/aave/aave-v3-core/blob/master/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+// import "https://github.com/aave/aave-v3-core/blob/master/contracts/interfaces/IPoolAddressesProvider.sol";
+// import "https://github.com/aave/aave-v3-core/blob/master/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 
-contract SimpleFlashLoan is FlashLoanSimpleReceiverBase {
-    address payable owner;
+// contract SimpleFlashLoan is FlashLoanSimpleReceiverBase {
+//     address payable owner;
 
-    event Log(string message, uint256 value);
+//     event Log(string message, uint256 value);
 
-    constructor(
-        address _addressProvider
-    ) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {}
+//     constructor(
+//         address _addressProvider
+//     ) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {}
 
-    function fn_RequestFlashLoan(address _token, uint256 _amount) public {
-        address receiverAddress = address(this);
-        address asset = _token;
-        uint256 amount = _amount;
-        bytes memory params = "";
-        uint16 referralCode = 0;
+//     function fn_RequestFlashLoan(address _token, uint256 _amount) public {
+//         address receiverAddress = address(this);
+//         address asset = _token;
+//         uint256 amount = _amount;
+//         bytes memory params = "";
+//         uint16 referralCode = 0;
 
-        POOL.flashLoanSimple(
-            receiverAddress,
-            asset,
-            amount,
-            params,
-            referralCode
-        );
-    }
+//         POOL.flashLoanSimple(
+//             receiverAddress,
+//             asset,
+//             amount,
+//             params,
+//             referralCode
+//         );
+//     }
 
-    //This function is called after your contract has received the flash loaned amount
+//     //This function is called after your contract has received the flash loaned amount
 
-    function executeOperation(
-        address asset,
-        uint256 amount,
-        uint256 premium,
-        address initiator,
-        bytes calldata params
-    ) external override returns (bool) {
-        //Logic goes here
+//     function executeOperation(
+//         address asset,
+//         uint256 amount,
+//         uint256 premium,
+//         address initiator,
+//         bytes calldata params
+//     ) external override returns (bool) {
+//         //Logic goes here
 
-        uint256 totalAmount = amount + premium;
+//         uint256 totalAmount = amount + premium;
 
-        uint256 balance = IERC20(asset).balanceOf(address(this));
-        emit Log("Balance before repayment", balance);
-        emit Log("Total to repay", totalAmount);
+//         uint256 balance = IERC20(asset).balanceOf(address(this));
+//         emit Log("Balance before repayment", balance);
+//         emit Log("Total to repay", totalAmount);
 
-        require(balance >= totalAmount, "Insufficient balance to repay");
+//         require(balance >= totalAmount, "Insufficient balance to repay");
 
-        IERC20(asset).approve(address(POOL), totalAmount);
+//         IERC20(asset).approve(address(POOL), totalAmount);
 
-        return true;
-    }
+//         return true;
+//     }
 
-    receive() external payable {}
-}
+//     receive() external payable {}
+// }
