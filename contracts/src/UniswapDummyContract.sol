78ba6f7ddedaa249024820f5ed1e7ea715258792
Constantino M <constantino@Constantinos-MacBook-Pro.local>
2025-06-03 01:47:31 -0600

    made current erc20 token a superchain erc20 token

--- contracts/src/UniswapDummyContract.sol
+++ contracts/src/UniswapDummyContract.sol
@@ -4,14 +4,55 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC7802, IERC165 } from "@interop-lib/interfaces/IERC7802.sol";
+import {PredeployAddresses} from "@interop-lib/libraries/PredeployAddresses.sol";
 
-contract TestUSDToken is ERC20 {
+contract TestUSDToken is IERC7802, ERC20 {
     constructor() ERC20("TestUSDToken", "TUSD") {
         //_mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1,000,000 TUSD to deployer
     }
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
+    }
+
+    /// @notice Allows the SuperchainTokenBridge to mint tokens.
+    /// @param _to     Address to mint tokens to.
+    /// @param _amount Amount of tokens to mint.
+    function crosschainMint(address _to, uint256 _amount) external {
+        // Only the `SuperchainTokenBridge` has permissions to mint tokens during crosschain transfers.
+        require(msg.sender == PredeployAddresses.SUPERCHAIN_TOKEN_BRIDGE, "Unauthorized");
+
+        // Mint tokens to the `_to` account's balance.
+        _mint(_to, _amount);
+
+        // Emit the CrosschainMint event included on IERC7802 for tracking token mints associated with cross chain transfers.
+        emit CrosschainMint(_to, _amount, msg.sender);
+    }
+
+    /// @notice Allows the SuperchainTokenBridge to burn tokens.
+    /// @param _from   Address to burn tokens from.
+    /// @param _amount Amount of tokens to burn.
+    function crosschainBurn(address _from, uint256 _amount) external {
+        // Only the `SuperchainTokenBridge` has permissions to burn tokens during crosschain transfers.
+        require(msg.sender == PredeployAddresses.SUPERCHAIN_TOKEN_BRIDGE, "Unauthorized");
+
+        // Burn the tokens from the `_from` account's balance.
+        _burn(_from, _amount);
+
+        // Emit the CrosschainBurn event included on IERC7802 for tracking token burns associated with cross chain transfers.
+        emit CrosschainBurn(_from, _amount, msg.sender);
+    }
+
+    /// @notice Query if a contract implements an interface
+    /// @param _interfaceId The interface identifier, as specified in ERC-165
+    /// @dev Interface identification is specified in ERC-165. This function
+    /// uses less than 30,000 gas.
+    /// @return `true` if the contract implements `interfaceID` and
+    /// `interfaceID` is not 0xffffffff, `false` otherwise
+    function supportsInterface(bytes4 _interfaceId) public view virtual returns (bool) {
+        return _interfaceId == type(IERC7802).interfaceId || _interfaceId == type(IERC20).interfaceId
+            || _interfaceId == type(IERC165).interfaceId;
     }
 }
 
